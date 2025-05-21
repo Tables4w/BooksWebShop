2dbc4b5c18abc83ff987e645a2b460484be3b9d6
Tablesaw <49002940+Tables4w@users.noreply.github.com>
2025-05-22 02:36:04 +0300

    Added General Catalog View

---
+++ mbooks/scripts/catalog.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
+from mbooks.models import *
+import json
+import base64
+from .specfunc import detect_image_type
+
+User=get_user_model();
+
+def catalog_back(request):
+    if request.method=='GET':
+        data = Book.objects.all()
+
+        books = []
+        for book in data:
+            genres = Genre.objects.filter(bookgenre__book=book).values_list('name', flat=True)
+            authors = Author.objects.filter(bookauthor__book=book).values_list('name', flat=True)
+            publishers = Publisher.objects.filter(bookpublisher__book=book).values_list('name', flat=True)
+
+            cover_image = ''
+            if book.cover:
+                img_type = detect_image_type(book.cover)
+                if img_type:
+                    cover_image = f'data:image/{img_type};base64,{base64.b64encode(book.cover).decode("utf-8")}'
+
+
+            books.append({
+                'id': book.id,
+                'title': book.name,
+                'description': book.description,
+                'year': book.publication_date.isoformat()[0:4],
+                'price': str(book.price),
+                'image': cover_image if cover_image else '',
+                'genre': list(genres) if genres else '',
+                'author': list(authors) if authors else '',
+                'publishers': list(publishers) if publishers else '',
+            })
+
+        books_json=json.dumps(books)
+
+        print('ok');
+
+        return render(request, 'mbooks/catalog.html', {'books_json':books_json})
--- mbooks/urls.py
+++ mbooks/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('main/', views.mainpage_view, name='main'),
     path('auth/', views.auth_view, name='auth'),
     path('profile/', views.profile_view, name='profile'),
+    path('catalog/', views.catalog_view, name='catalog'),
 ]
