29d4a2a26a30a1ca0238d4761dc89f46974802a3
Tablesaw <49002940+Tables4w@users.noreply.github.com>
2025-06-07 17:41:43 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from decouple import config
 from pathlib import Path
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
